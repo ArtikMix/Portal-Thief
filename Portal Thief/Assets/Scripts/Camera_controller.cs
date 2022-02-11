de8e3f44e4495786f24127fa6987e95534aff98e
ArtikMix <a.katkovski5@gmail.com>
2022-02-11 17:51:49 +0300

    camera mouse control, player rotation start

--- Portal Thief/Assets/Scripts/Camera_controller.cs
+++ Portal Thief/Assets/Scripts/Camera_controller.cs
@@ -18,7 +18,7 @@ public class Camera_controller : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            cam_pos.position = new Vector3(player.position.x, cam_pos.position.y, player.position.z - 45.5f);
+            cam_pos.position = new Vector3(player.position.x, cam_pos.position.y, player.position.z - cam_pos.position.y);
         }
         MouseNCameraLogic();
         mouse_pos = Input.mousePosition;
@@ -28,23 +28,17 @@ public class Camera_controller : MonoBehaviour
     {
         if (mouse_pos.x >= Screen.width)
         {
-            Debug.Log(0);
             cam_pos.transform.Translate(15f * Time.deltaTime, 0f, 0f);
         }
         if (mouse_pos.x <= 0)
         {
-            Debug.Log(1);
             cam_pos.transform.Translate(-15f * Time.deltaTime, 0f, 0f);
         }
-        /*if (mouse_pos.y >= Screen.height)
+        if (mouse_pos.y >= Screen.height)
         {
-            Debug.Log(2);
-            cam_pos.transform.Translate(0f, -15f * Time.deltaTime, 15f * Time.deltaTime);
         }
         if (mouse_pos.y <= 0)
         {
-            Debug.Log(3);
-            cam_pos.transform.Translate(0f, 15f * Time.deltaTime, -15f * Time.deltaTime);
-        }*/
+        }
     }
 }
--- Portal Thief/Assets/Scripts/Player_movement.cs
+++ Portal Thief/Assets/Scripts/Player_movement.cs
@@ -4,17 +4,40 @@ using UnityEngine;
 
 public class Player_movement : MonoBehaviour
 {
-    private CharacterController player;
+    private CharacterController player_controller;
+    private Transform player_transform;
     [HideInInspector] public float player_speed = 5f;
 
     private void Start()
     {
-        player = GetComponent<CharacterController>();
+        player_controller = GetComponent<CharacterController>();
+        player_transform = GetComponent<Transform>();
     }
 
     private void Update()
     {
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-        player.Move(move * player_speed * Time.deltaTime);
+        player_controller.Move(move * player_speed * Time.deltaTime);
+        RotateLogic();
+    }
+
+    private void RotateLogic()
+    {
+        if (Input.GetAxis("Horizontal") > 0f)
+        {
+            player_transform.rotation = Quaternion.Euler(0f, 90f, 0f);
+        }
+        if (Input.GetAxis("Horizontal") < 0f)
+        {
+            player_transform.rotation = Quaternion.Euler(0f, 270f, 0f);
+        }
+        if (Input.GetAxis("Vertical") > 0f)
+        {
+            player_transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+        }
+        if (Input.GetAxis("Vertical") > 0f)
+        {
+            player_transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+        }
     }
 }
