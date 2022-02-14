94418e9799be04a92c9beb1ae60a86c328c02ae3
ArtikMix <a.katkovski5@gmail.com>
2022-02-14 23:44:59 +0300

    enemy search raycast system

--- Portal Thief/Assets/Scripts/Enemy_controller.cs
+++ Portal Thief/Assets/Scripts/Enemy_controller.cs
@@ -6,12 +6,21 @@ using UnityEngine.AI;
 public class Enemy_controller : MonoBehaviour
 {
     private NavMeshAgent agent;
+    //патрулирование
     [SerializeField] private Transform[] patrolPoints;
     private int patrolPointNumber = -1;
     private bool onceStartCoroutine = false;
+    //поиск
+    [SerializeField] private float lampDistance = 10f;
+    [SerializeField] private float visionDistance = 15f;
+    [SerializeField] private float angle = 70f;
+    private Vector3 offset;
+    private Transform target;
+    private int raysCount = 100;
 
     private void Start()
     {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
         agent = GetComponent<NavMeshAgent>();
         Patrol();
     }
@@ -19,8 +28,9 @@ public class Enemy_controller : MonoBehaviour
     private void Update()
     {
         CheckPatrolPosition();
+        Search();
     }
-
+    #region Patrol
     private void Patrol()
     {
         if (patrolPointNumber != patrolPoints.Length-1)
@@ -54,4 +64,58 @@ public class Enemy_controller : MonoBehaviour
         yield return new WaitForSeconds(1.5f);
         Patrol();
     }
+    #endregion
+    private void EnemyVision()
+    {
+
+    }
+    #region Search
+    private int GetRaycast(Vector3 dir)
+    {
+        int result = 0;
+        RaycastHit hit = new RaycastHit();
+        Vector3 pos = transform.position + offset;
+        if (Physics.Raycast(pos, dir, out hit, visionDistance))
+        {
+            
+            if (hit.transform == target && Vector3.Distance(transform.position, target.position)<=lampDistance)
+            {
+                result = 1;
+                Debug.Log("Enemy " + transform.name + " has find PLAYER at 10 (lampDistance)");
+                Debug.DrawLine(transform.position, hit.point, Color.red);
+                return result;
+            }
+            if (hit.transform == target && Vector3.Distance(transform.position, target.position)<=visionDistance && Vector3.Distance(transform.position, target.position) > lampDistance)
+            {
+                result = 2;
+                Debug.Log("Enemy " + transform.name + " has find PLAYER at 15 (visionDistance)");
+                Debug.DrawLine(transform.position, hit.point, Color.yellow);
+                return result;
+            }
+        }
+        return result;
+    }
+
+    private int Search()
+    {
+        float j = 0;
+        for (int i = 0; i < raysCount; i++)
+        {
+            var x = Mathf.Sin(j);
+            var y = Mathf.Cos(j);
+
+            j += angle * Mathf.Deg2Rad / raysCount;
+
+            Vector3 dir = transform.TransformDirection(new Vector3(x, 0, y));
+
+            switch (GetRaycast(dir))
+            {
+                case 0: continue;
+                case 1: return 1;
+                case 2: return 2;
+            }
+        }
+        return 0;
+    }
+    #endregion
 }
